f68189a571b82582dc3947ddf6b4fe49fd299962
Steeve Morin <steeve.morin@gmail.com>
2013-12-02 20:34:16 +0100

    Refactor to perform libtorrent call from only main thread

--- torrent2http.go
+++ torrent2http.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-    "fmt"
     "net/http"
     "path"
     "os"
@@ -12,144 +11,189 @@ import (
     "math"
     "encoding/json"
     "github.com/steeve/libtorrent-go"
+    "runtime"
+    "fmt"
 )
 
-type JSONStruct map[string]interface{}
+type FileStatusInfo struct {
+    Name            string  `json:"name"`
+    Size            int64   `json:"size"`
+    Offset          int64   `json:"offset"`
+    TotalPieces     int     `json:"total_pieces"`
+    CompletePieces  int     `json:"complete_pieces"`
+}
 
-func (r JSONStruct) String() (s string) {
-    b, err := json.Marshal(r)
-    if err != nil {
-        s = ""
-        return
-    }
-    s = string(b)
-    return
+type LsInfo struct {
+    Files   []FileStatusInfo    `json:"files"`
+}
+
+type SessionStatus struct {
+    State           int     `json:"state"`
+    Progress        float32 `json:"progress"`
+    DownloadRate    float32 `json:"download_rate"`
+    UploadRate      float32 `json:"upload_rate"`
+    NumPeers        int     `json:"num_peers"`
+    NumSeeds        int     `json:"num_seeds"`
 }
 
 type Config struct {
     magnetUri           string
     bindAddress         string
-    max_upload_rate     int
-    max_download_rate   int
-    download_path       string
-    keep_files          bool
-    min_memory_mode     bool
+    maxUploadRate       int
+    maxDownloadRate     int
+    downloadPath        string
+    keepFiles           bool
     encryption          int
+    noSparseFile        bool
 }
 
-var config Config
-var session libtorrent.Session
-var torrentHandle libtorrent.Torrent_handle
-var tfs *TorrentFS
+type Instance struct {
+    config           Config
+    session          libtorrent.Session
+    torrentHandle    libtorrent.Torrent_handle
+    torrentFS        *TorrentFS
+}
+
+var instance = Instance{}
+var mainFuncChan = make(chan func())
+
+func runInMainThread(f interface{}) interface{} {
+    done := make(chan interface{}, 1)
+    mainFuncChan <- func() {
+        switch f := f.(type) {
+        case func():
+            f()
+            done <- true
+        case func() interface{}:
+            done <- f()
+        }
+    }
+    return <-done
+}
 
 func statusHandler(w http.ResponseWriter, r *http.Request) {
     w.Header().Set("Content-Type", "application/json")
-    if torrentHandle == nil {
-        fmt.Fprint(w, JSONStruct{"state": -1})
-        return
-    }
-    status := torrentHandle.Status()
 
-    fmt.Fprint(w, JSONStruct{
-        "state": status.GetState(),
-        "progress": status.GetProgress(),
-        "download_rate": float32(status.GetDownload_rate()) / 1000,
-        "upload_rate": float32(status.GetUpload_rate()) / 1000,
-        "num_peers": status.GetNum_peers(),
-        "num_seeds": status.GetNum_seeds()})
+    var status SessionStatus
+    if instance.torrentHandle == nil {
+        status = SessionStatus{State: -1}
+    } else {
+        runInMainThread(func() {
+            tstatus := instance.torrentHandle.Status()
+            status = SessionStatus{
+                State: int(tstatus.GetState()),
+                Progress: tstatus.GetProgress(),
+                DownloadRate: float32(tstatus.GetDownload_rate()) / 1000,
+                UploadRate: float32(tstatus.GetUpload_rate()) / 1000,
+                NumPeers: tstatus.GetNum_peers(),
+                NumSeeds: tstatus.GetNum_seeds()}
+        })
+    }
+
+    output, _ := json.Marshal(status)
+    w.Write(output)
 }
 
 func lsHandler(w http.ResponseWriter, r *http.Request) {
     w.Header().Set("Content-Type", "application/json")
 
-    dir, _ := tfs.TFSOpen("/")
+    dir, _ := instance.torrentFS.TFSOpen("/")
     files, _ := dir.TFSReaddir(-1)
-    retFiles := make([]JSONStruct, len(files))
-    for i, file := range files {
+    retFiles := LsInfo{}
+
+    for _, file := range files {
         startPiece, endPiece := file.Pieces()
-        retFiles[i] = JSONStruct{
-            "name": file.Name(),
-            "size": file.Size(),
-            "offset": file.Offset(),
-            "total_pieces": int(math.Max(float64(endPiece - startPiece), 1)),
-            "complete_pieces": file.CompletedPieces()}
+
+        fi := FileStatusInfo{
+            Name: file.Name(),
+            Size: file.Size(),
+            Offset: file.Offset(),
+            TotalPieces: int(math.Max(float64(endPiece - startPiece), 1)),
+            CompletePieces: file.CompletedPieces()}
+        retFiles.Files = append(retFiles.Files, fi)
     }
 
-    fmt.Fprint(w, JSONStruct{"files": retFiles})
+    output, _ := json.Marshal(retFiles)
+    w.Write(output)
 }
 
 func startServices() {
     log.Println("Starting DHT...")
-    session.Start_dht()
+    instance.session.Start_dht()
 
     log.Println("Starting LSD...")
-    session.Start_lsd()
+    instance.session.Start_lsd()
 
     log.Println("Starting UPNP...")
-    session.Start_upnp()
+    instance.session.Start_upnp()
 
     log.Println("Starting NATPMP...")
-    session.Start_natpmp()
+    instance.session.Start_natpmp()
 }
 
 func stopServices() {
     log.Println("Stopping DHT...")
-    session.Stop_dht()
+    instance.session.Stop_dht()
 
     log.Println("Stopping LSD...")
-    session.Stop_lsd()
+    instance.session.Stop_lsd()
 
     log.Println("Stopping UPNP...")
-    session.Stop_upnp()
+    instance.session.Stop_upnp()
 
     log.Println("Stopping NATPMP...")
-    session.Stop_natpmp()
+    instance.session.Stop_natpmp()
 }
 
 func removeFiles() {
-    if torrentHandle.Status().GetHas_metadata() == false {
+    if instance.torrentHandle.Status().GetHas_metadata() == false {
         return
     }
 
-    torrentInfo := torrentHandle.Get_torrent_info()
+    torrentInfo := instance.torrentHandle.Get_torrent_info()
     for i := 0; i < torrentInfo.Num_files(); i++ {
-        os.RemoveAll(path.Join(torrentHandle.Save_path(), torrentInfo.File_at(i).GetPath()))
+        os.RemoveAll(path.Join(instance.torrentHandle.Save_path(), torrentInfo.File_at(i).GetPath()))
     }
 }
 
-func cleanup() {
-    stopServices()
+func shutdown() {
+    runInMainThread(func () {
+        log.Println("Stopping torrent2http...")
 
-    log.Println("Removing torrent...")
+        stopServices()
 
-    if config.keep_files == true {
-        return
-    }
+        log.Println("Removing torrent...")
 
-    session.Set_alert_mask(libtorrent.AlertStorage_notification)
-    // Just in case
-    defer removeFiles()
-    session.Remove_torrent(torrentHandle, 1);
-    log.Println("Waiting for files to be removed...")
-    for {
-        if session.Wait_for_alert(libtorrent.Seconds(30)).Swigcptr() == 0 {
-            return
+        if instance.config.keepFiles == false {
+            instance.session.Set_alert_mask(libtorrent.AlertStorage_notification)
+            // Just in case
+            defer removeFiles()
+            instance.session.Remove_torrent(instance.torrentHandle, 1);
+            log.Println("Waiting for files to be removed...")
+            for {
+                if instance.session.Wait_for_alert(libtorrent.Seconds(30)).Swigcptr() == 0 {
+                    break
+                }
+                if instance.session.Pop_alert2().What() == "cache_flushed_alert" {
+                    break
+                }
+            }
         }
-        if session.Pop_alert2().What() == "cache_flushed_alert" {
-            return
-        }
-    }
+
+        log.Println("Bye bye")
+        os.Exit(0)
+    })
 }
 
 func parseFlags() {
-    config = Config{}
+    config := Config{}
     flag.StringVar(&config.magnetUri, "magnet", "", "Magnet URI")
     flag.StringVar(&config.bindAddress, "bind", ":5001", "Bind address of torrent2http")
-    flag.IntVar(&config.max_download_rate, "dlrate", 0, "Max Download Rate")
-    flag.IntVar(&config.max_upload_rate, "ulrate", 0, "Max Upload Rate")
-    flag.StringVar(&config.download_path, "dlpath", ".", "Download path")
-    flag.BoolVar(&config.keep_files, "keep", false, "Keep files after exiting")
-    flag.BoolVar(&config.min_memory_mode, "minmem", false, "Min memory mode (for embedded platforms such as Raspberry Pi)")
+    flag.IntVar(&config.maxDownloadRate, "dlrate", 0, "Max Download Rate")
+    flag.IntVar(&config.maxUploadRate, "ulrate", 0, "Max Upload Rate")
+    flag.StringVar(&config.downloadPath, "dlpath", ".", "Download path")
+    flag.BoolVar(&config.keepFiles, "keep", false, "Keep files after exiting")
+    flag.BoolVar(&config.noSparseFile, "no-sparse", false, "Do not use sparse file allocation.")
     flag.IntVar(&config.encryption, "encryption", 1, "Encryption: 0=forced 1=enabled (default) 2=disabled")
     flag.Parse()
 
@@ -157,150 +201,103 @@ func parseFlags() {
         flag.Usage();
         os.Exit(1)
     }
+
+    instance.config = config
 }
 
-// Go version of libtorrent.Min_memory_usage in case we need to tweak
-func getMimMemorySettings() libtorrent.Session_settings {
-    set := session.Settings()
+func configureSession() {
+    settings := instance.session.Settings()
 
-    set.SetAlert_queue_size(100);
+    log.Println("Setting Session settings...")
+    settings.SetConnection_speed(1000)
+    settings.SetRequest_timeout(5)
+    settings.SetPeer_connect_timeout(2)
+    if instance.config.maxDownloadRate > 0 {
+        settings.SetDownload_rate_limit(instance.config.maxDownloadRate * 1024)
+    }
+    if instance.config.maxUploadRate > 0 {
+        settings.SetUpload_rate_limit(instance.config.maxUploadRate * 1024)
+    }
+    instance.session.Set_settings(settings)
 
-    // setting this to a low limit, means more
-    // peers are more likely to request from the
-    // same piece. Which means fewer partial
-    // pieces and fewer entries in the partial
-    // piece list
-    set.SetWhole_pieces_threshold(2)
-    set.SetUse_parole_mode(false)
-    set.SetPrioritize_partial_pieces(true)
+    log.Println("Setting Encryption settings...")
+    encryptionSettings := libtorrent.NewPe_settings()
+    encryptionSettings.SetOut_enc_policy(libtorrent.LibtorrentPe_settingsEnc_policy(instance.config.encryption))
+    encryptionSettings.SetIn_enc_policy(libtorrent.LibtorrentPe_settingsEnc_policy(instance.config.encryption))
+    encryptionSettings.SetAllowed_enc_level(libtorrent.Pe_settingsBoth)
+    encryptionSettings.SetPrefer_rc4(true)
+    instance.session.Set_pe_settings(encryptionSettings)
+}
 
-    // connect to 5 peers per second
-    set.SetConnection_speed(5)
+func startHTTP() {
+    log.Println("Starting HTTP Server...")
+    http.HandleFunc("/status", statusHandler)
+    http.HandleFunc("/ls", lsHandler)
+    http.Handle("/files/", http.StripPrefix("/files/", http.FileServer(instance.torrentFS)))
+    http.HandleFunc("/shutdown", func (w http.ResponseWriter, r *http.Request) {
+        go shutdown()
+        fmt.Fprintf(w, "OK")
+    })
 
-    // be extra nice on the hard drive when running
-    // on embedded devices. This might slow down
-    // torrent checking
-    set.SetFile_checks_delay_per_block(5)
-
-    // only have 4 files open at a time
-    set.SetFile_pool_size(4)
-
-    // we want to keep the peer list as small as possible
-    set.SetAllow_multiple_connections_per_ip(false)
-    set.SetMax_failcount(2)
-    set.SetInactivity_timeout(120)
-
-    // whenever a peer has downloaded one block, write
-    // it to disk, and don't read anything from the
-    // socket until the disk write is complete
-    set.SetMax_queued_disk_bytes(1)
-
-//===================================================================
-
-    // don't keep track of all upnp devices, keep
-    // the device list small
-    set.SetUpnp_ignore_nonrouters(true)
-
-    // never keep more than one 16kB block in
-    // the send buffer
-    set.SetSend_buffer_watermark(9)
-
-    // don't use any disk cache
-    // set.SetCache_size(0)
-    // set.SetCache_buffer_chunk_size(1)
-    // set.SetUse_read_cache(false)
-    // set.SetUse_disk_read_ahead(false)
-
-    set.SetClose_redundant_connections(true)
-
-    set.SetMax_peerlist_size(500)
-    set.SetMax_paused_peerlist_size(50)
-
-    // udp trackers are cheaper to talk to
-    set.SetPrefer_udp_trackers(true)
-
-    set.SetMax_rejects(10)
-
-    set.SetRecv_socket_buffer_size(16 * 1024)
-    set.SetSend_socket_buffer_size(16 * 1024)
-
-    // use less memory when checking pieces
-    // set.SetOptimize_hashing_for_speed(false)
-
-    // use less memory when reading and writing
-    // whole pieces
-    set.SetCoalesce_reads(false)
-    set.SetCoalesce_writes(false)
-
-    // disallow the buffer size to grow for the uTP socket
-    set.SetUtp_dynamic_sock_buf(false)
-
-    return set
+    log.Printf("Listening HTTP on %s...\n", instance.config.bindAddress)
+    http.ListenAndServe(instance.config.bindAddress, nil)
 }
 
 func main() {
+    // Make sure we are properly multithreaded, on a minimum of 2 threads
+    // because we lock the main thread for libtorrent.
+    maxProcs := runtime.NumCPU()
+    if maxProcs < 2 {
+        maxProcs = 2
+    }
+    runtime.GOMAXPROCS(maxProcs)
+
+    // Lock the main thread.
+    runtime.LockOSThread()
+    defer runtime.UnlockOSThread()
+
     parseFlags()
 
     log.Println("Starting BT engine...")
-    session = libtorrent.NewSession()
+    instance.session = libtorrent.NewSession()
+    instance.session.Listen_on(libtorrent.NewPair_int_int(6900, 6999))
 
-    session.Listen_on(libtorrent.NewPair_int_int(6900, 6999))
-
-    log.Println("Setting Session settings...")
-    sessionSettings := session.Settings()
-    if config.min_memory_mode == true {
-        sessionSettings = libtorrent.Min_memory_usage()
-        sessionSettings.SetMax_queued_disk_bytes(64 * 1024)
-    }
-    sessionSettings.SetConnection_speed(1000)
-    sessionSettings.SetRequest_timeout(5)
-    sessionSettings.SetPeer_connect_timeout(2)
-    if config.max_download_rate > 0 {
-        sessionSettings.SetDownload_rate_limit(config.max_download_rate * 1024)
-    }
-    if config.max_upload_rate > 0 {
-        sessionSettings.SetUpload_rate_limit(config.max_upload_rate * 1024)
-    }
-    session.Set_settings(sessionSettings)
-
-    // Set encryption settings
-    encryptionSettings := libtorrent.NewPe_settings()
-    encryptionSettings.SetOut_enc_policy(libtorrent.LibtorrentPe_settingsEnc_policy(config.encryption))
-    encryptionSettings.SetIn_enc_policy(libtorrent.LibtorrentPe_settingsEnc_policy(config.encryption))
-    encryptionSettings.SetAllowed_enc_level(libtorrent.Pe_settingsBoth)
-    encryptionSettings.SetPrefer_rc4(true)
-    session.Set_pe_settings(encryptionSettings)
+    configureSession()
 
     startServices()
 
-    torrentParams := libtorrent.Parse_magnet_uri2(config.magnetUri)
-    torrentParams.SetSave_path(config.download_path)
-    torrentHandle = session.Add_torrent(torrentParams)
-    torrentHandle.Set_sequential_download(true)
+    log.Println("Parsing magnet link")
+    torrentParams := libtorrent.Parse_magnet_uri2(instance.config.magnetUri)
+
+    log.Println("Setting save path")
+    torrentParams.SetSave_path(instance.config.downloadPath)
+
+    if instance.config.noSparseFile {
+        log.Println("Disabling sparse file support...")
+        torrentParams.SetStorage_mode(libtorrent.Storage_mode_allocate)
+    }
+
+    log.Println("Adding torrent")
+    instance.torrentHandle = instance.session.Add_torrent(torrentParams)
+
+    log.Println("Enabling sequential download")
+    instance.torrentHandle.Set_sequential_download(true)
+
     log.Printf("Downloading: %s\n", torrentParams.GetName())
 
-    tfs = NewTorrentFS(torrentHandle)
+    instance.torrentFS = NewTorrentFS(instance.torrentHandle)
 
-    log.Println("Registering HTTP endpoints...")
-    http.HandleFunc("/status", statusHandler)
-    http.HandleFunc("/ls", lsHandler)
-    http.Handle("/files/", http.StripPrefix("/files/", http.FileServer(tfs)))
+    go startHTTP()
 
-    // Shutdown procedures
-    c := make(chan os.Signal, 1)
-    signal.Notify(c, os.Interrupt, syscall.SIGTERM)
-    // Allow shutdown via HTTP
-    http.HandleFunc("/shutdown", func (w http.ResponseWriter, r *http.Request) {
-        c <- os.Interrupt
-    })
-    go func(){
-        <-c
-        log.Println("Stopping torrent2http...")
-        cleanup()
-        log.Println("Bye bye")
-        os.Exit(0)
+    // Handle SIGTERM (Ctrl-C)
+    go func() {
+        signalChan := make(chan os.Signal, 1)
+        signal.Notify(signalChan, os.Interrupt, syscall.SIGTERM)
+        <- signalChan
+        go shutdown()
     }()
 
-    log.Printf("Listening HTTP on %s...\n", config.bindAddress)
-    http.ListenAndServe(config.bindAddress, nil)
+    for f := range mainFuncChan {
+        f()
+    }
 }
